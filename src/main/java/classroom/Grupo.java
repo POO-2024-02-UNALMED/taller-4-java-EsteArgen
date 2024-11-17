b44282e83ab746767ae499884546294a9f0f03ef
EsteArgen <101402836+EsteArgen@users.noreply.github.com>
2024-11-16 22:22:30 -0500

    Update Grupo.java

--- src/main/java/classroom/Grupo.java
+++ src/main/java/classroom/Grupo.java
@@ -15,7 +15,7 @@ public class Grupo {
         this.codigo = codigo;
         this.horario = horario;
     }
-
+ 
     public Grupo(int cantidadEstudiantes, Persona profesor, Asignatura asignatura, int codigo, String horario) {
         Persona[] personas = new Persona[cantidadEstudiantes];
         this(personas, profesor, asignatura, codigo, horario);
